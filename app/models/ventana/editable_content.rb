39fa32b4c30dadb718b367771e2263abf151b55c
Mason Fischer <mason@thoughtbot.com>
2012-10-12 11:03:11 -0700

    Default to text type

--- app/models/ventana/editable_content.rb
+++ app/models/ventana/editable_content.rb
@@ -2,15 +2,15 @@ require 'redcarpet'
 
 module Ventana
   class EditableContent < ActiveRecord::Base
-    attr_accessible :key, :cotent_type, :value
+    attr_accessible :key, :content_type, :value
     def render
-      case content_type.to_sym
-      when :text
-        value.to_s
+      case content_type.to_s.to_sym
       when :markdown
         markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
           :autolink => true, :space_after_headers => true)
         markdown.render(value.to_s)
+      else
+        value.to_s
       end
     end
 
